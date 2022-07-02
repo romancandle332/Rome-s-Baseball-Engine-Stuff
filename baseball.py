a7a6ee5b83760cd5480258a0162bd55c849bec71
romancandle332 <romancandle332@gmail.com>
2022-07-02 17:20:44 -0400

    Change from Function to Class

--- baseball.py
+++ baseball.py
@@ -1,178 +1,46 @@
 import math
 import random
 
-def GetPitch(x,y,r): ##library of different pitch attributes. x is what pitch you want, y is what attribute you want outputted, r is pitch throwing hand
-    if x == "4SM":
-        pitch_name = "4-Seam Fastball"
-        pitch_type = "Fastball"
-        mph_avg = 93.51
-        mph_dev = 2.27
-        whiff_avg = 0.211
-        whiff_dev = 0.053
-        movement = [-1,-1] ##Measured against GYR; Positive = Down and Right from Batter
+class Pitch:
+    def __init__(self,a,b,c,d,e,f,g):
+        self.name = a
+        self.group = b
+        self.mph_avg = c
+        self.mph_dev = d
+        self.whiff_avg = e
+        self.whiff_dev = f
+        self.movement = g
 
-    elif x == "CUT":
-        pitch_name = "Cut Fastball"
-        pitch_type = "Fastball"
-        mph_avg = 88.55
-        mph_dev = 2.53
-        whiff_avg = 0.251
-        whiff_dev = 0.075
-        movement = [1,1] ##Measured against GYR; Positive = Down and Right from Batter      
+FSM = Pitch("4-Seam Fastball","Fastball",93.51,2.27,0.211,0.053,[-1,-1])
+CUT = Pitch("Cut Fastball","Fastball",88.55,2.53,0.251,0.075,[1,1])
+TSM = Pitch("2-Seam Fastball","Fastball",88.55,2.53,0.251,0.075,[1,-1])
+SPT = Pitch("Split-Finger Fastball","Fastball",85.78,2.30,0.375,0.71,[3,0])
+SNK = Pitch("Sinking Fastball","Fastball",92.56,2.55,0.148,0.040,[2,1])
+CHN = Pitch("Change-Up","Offspeed",84.58,2.93,0.305,0.066,[2,0])
+CIR = Pitch("Circle Change","Offspeed",80.58,2.93,0.305,0.66,[2,-1])
+PLM = Pitch("Palmball","Offspeed",78.58,2.93,0.305,0.066,[3,1])
+EPH = Pitch("Eephus","Offspeed",65.18,3.85,0.242,0.104,[1,1])
+GYR = Pitch("Gyro Bullet","Offspeed",82.58,3.43,0.211,0.111,[0,0])
+SLD = Pitch("Slider","Breaking",84.65,2.89,0.357,0.76,[2,5])
+CRV = Pitch("Curveball","Breaking",78.72,3.28,0.320,0.075,[6,2])
+TSC = Pitch("12-6 Curveball","Breaking",75.72,3.28,0.320,0.075,[7,0])
+SLV = Pitch("Slurve","Breaking",70.60,4.16,0.261,0.069,[4,4])
+SCR = Pitch("Screwball","Breaking",73.41,4.18,0.371,0.091,[6,-2])
+FOR = Pitch("Forkball","Breaking",82.48,3.41,0.287,0.097,[4,0])
+KCV = Pitch("Knuckle-Curve","Breaking",79.64,2.98,0.297,0.110,[4,1])
+KNB = Pitch("Knuckleball","Breaking",71.92,4.22,0.223,0.028,[round(random.uniform(-4,6)),round(random.uniform(-4,6))])
 
-    elif x == "2SM":
-        pitch_name = "2-Seam Fastball"
-        pitch_type = "Fastball"
-        mph_avg = 88.55
-        mph_dev = 2.53
-        whiff_avg = 0.251
-        whiff_dev = 0.075
-        movement = [1,-1] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "SPT":
-        pitch_name = "Split-Finger Fasterball"
-        pitch_type = "Fastball"
-        mph_avg = 85.78
-        mph_dev = 2.30
-        whiff_avg = 0.375
-        whiff_dev = 0.071
-        movement = [3,0] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "SNK":
-        pitch_name = "Sinking Fastball"
-        pitch_type = "Fastball"
-        mph_avg = 92.56
-        mph_dev = 2.55
-        whiff_avg = 0.148
-        whiff_dev = 0.040
-        movement = [2,1] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "CHN":
-        pitch_name = "Change-Up"
-        pitch_type = "Offspeed"
-        mph_avg = 84.58
-        mph_dev = 2.93
-        whiff_avg = 0.305
-        whiff_dev = 0.066
-        movement = [2,0] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "CIR":
-        pitch_name = "Circle Change"
-        pitch_type = "Offspeed"
-        mph_avg = 80.58
-        mph_dev = 2.93
-        whiff_avg = 0.305
-        whiff_dev = 0.066
-        movement = [2,-1] ##Measured against GYR; Positive = Down and Right from Batter
-
-
-    elif x == "PLM":
-        pitch_name = "Palmball"
-        pitch_type = "Offspeed"
-        mph_avg = 78.58
-        mph_dev = 2.93
-        whiff_avg = 0.305
-        whiff_dev = 0.066
-        movement = [3,1] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "SLD":
-        pitch_name = "Slider" 
-        pitch_type = "Breaking"
-        mph_avg = 84.65
-        mph_dev = 2.89
-        whiff_avg = 0.357
-        whiff_dev = 0.76
-        movement = [2,5] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "CRV":
-        pitch_name = "Curveball"
-        pitch_type = "Breaking"
-        mph_avg = 78.72
-        mph_dev = 3.28
-        whiff_avg = 0.320
-        whiff_dev = 0.075
-        movement = [6,2] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "126":
-        pitch_name = "12-6 Curveball"
-        pitch_type = "Breaking"
-        mph_avg = 75.72
-        mph_dev = 3.28
-        whiff_avg = 0.320
-        whiff_dev = 0.075
-        movement = [7,0] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "SLV":
-        pitch_name = "Slurve"
-        pitch_type = "Breaking"
-        mph_avg = 70.60
-        mph_dev = 4.16
-        whiff_avg = 0.261
-        whiff_dev = 0.069
-        movement = [4,4] ##Measured against GYR; Positive = Down and Right from Batter
-
-
-    elif x == "SCR":
-        pitch_name = "Screwball"
-        pitch_type = "Breaking"
-        mph_avg = 73.41
-        mph_dev = 4.18
-        whiff_avg = 0.371
-        whiff_dev = 0.091
-        movement = [6,-2] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "FOR":
-        pitch_name = "Forkball"
-        pitch_type = "Breaking"
-        mph_avg = 82.48
-        mph_dev = 3.41
-        whiff_avg = 0.287
-        whiff_dev = 0.097
-        movement = [4,0] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "KCV":
-        pitch_name = "Knuckle-Curve"
-        pitch_type = "Breaking"
-        mph_avg = 79.64
-        mph_dev = 2.98
-        whiff_avg = 0.297
-        whiff_dev = 0.110
-        movement = [4,1] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "KNB":
-        pitch_name = "Knuckleball"
-        pitch_type = "Breaking"
-        mph_avg = 71.92
-        mph_dev = 4.22
-        whiff_avg = .223
-        whiff_dev = .028
-        vert = round(random.uniform(-4,6))
-        horz = round(random.uniform(-4,6))
-        movement = [vert,horz] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "EPH":
-        pitch_name = "Eephus"
-        pitch_type = "Offspeed"
-        mph_avg = 65.18
-        mph_dev = 3.85
-        whiff_avg = 0.242
-        whiff_dev = 0.104
-        horz = random.randint(0,1)
-        roll = random.randint(0,1)
-        if roll == 1:
-            horz *= -1
-        movement = [horz,1] ##Measured against GYR; Positive = Down and Right from Batter
-
-    elif x == "GYR":
-        pitch_name = "Gyro Bullet"
-        pitch_type = "Offspeed"
-        mph_avg = 82.58
-        mph_dev = 3.43
-        whiff_avg = 0.211
-        whiff_dev = 0.111
-        movement = [0,0] ##Measured against GYR; Positive = Down and Right from Batter
-
+def GetPitch(x,y,h):
     if y == "movement":
-        if r == 1: ##LHP
-            movement[1] *= -1
-    return y
+        if h == "1": ##LHP
+            q = x.movement[1]
+            z = x.movement[0]
+            q *= -1
+            result = [z,q]
+            return result
+        else:
+            result = x.movement
+            return result
+    else:
+        result = x.y
+        return result
---
+++ test.py
@@ -0,0 +1,7 @@
+import baseball
+
+y = 0
+while y < 5:
+    a = baseball.KNB.movement
+    print(a)
+    y += 1
